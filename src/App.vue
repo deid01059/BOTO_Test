f9dd03395a5e16085163ecc44826bae7352804e1
deid01059 <deid01059@gmail.com>
2024-04-30 18:35:55 +0900

    0430 보토테스트

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <MainComponent/>
+</template>
+
+<script>
+import MainComponent from './components/MainComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainComponent
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card"  @click="flipCard">
+        <div :class="card.flip ? 'back' : 'front'"></div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'OpenComponent',
+
+    data() {
+        return {
+            card: {
+                flip:false
+            }
+        }
+    },
+    methods: {
+        // 카드 뒤집는 메소드
+        flipCard(){
+            this.card.flip = !this.card.flip
+        }
+    }
+}
+</script>
+
+<style lang="/public/css/common.css">
+
+</style>
